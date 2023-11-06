0a94c6fd2ef30cdf5f213d40f891e4066c0169eb
Mohammad Shamami <mosh@Mohammads-MacBook-Pro.local>
2023-11-06 17:06:10 +0330

    resolved configuration issue

--- lib/initializer/mellat.rb
+++ lib/initializer/mellat.rb
@@ -3,6 +3,6 @@
 Mellat.configure do |config|
   config.terminal_id = '1527916'
   config.proxy = 'http://148.251.76.196:1088'
-  config.username = 'takhfi3'
-  config.password = '82968874'
+  config.username = 'username'
+  config.password = 'password'
 end
